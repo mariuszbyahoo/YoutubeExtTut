c6dfe3934237ec3fa33ce949f21c0d5bdc9c411d
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-11-22 11:50:44 +0100

    #8 Create Store

--- classic/src/view/main/Extra.js
+++ classic/src/view/main/Extra.js
@@ -2,15 +2,8 @@ Ext.define('YoutubeExtTut.view.main.Extra', {
     extend: 'Ext.grid.Panel', 
     xtype: 'extra',
     title: 'grid video',
-    store: [
-        { name: 'Mariusz Budzisz', email:'mbudzisz@mail.com', phone:'123456789'},
-        { name: 'Jean Luc', email: "jeanluc.picard@enterprise.com", phone: "555-111-1111"},
-        { name: 'Worf',     email: "worf.moghsson@enterprise.com",  phone: "555-222-2222"},
-        { name: 'Deanna',   email: "deanna.troi@enterprise.com",    phone: "555-333-3333"},
-        { name: 'Data',     email: "mr.data@enterprise.com",        phone: "555-444-4444"},
-    ],
-    // somethin is wrong here, app recognizes the number of 5 entries but
-    // the data fields are empty in the grid.
+    store: {type: 'extra'},
+
     columns: [
         {text: 'Name', dataIndex:'name', flex: 1},
         {text: 'e-mail', dataIndex:'email', flex: 1},
